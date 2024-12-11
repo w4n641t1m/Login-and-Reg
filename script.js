5a98b94de40ec59bd7b07bc7ba24b341da7b9304
w4n641t1m <168165505+w4n641t1m@users.noreply.github.com>
2024-12-11 11:23:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const wrapper = document.querySelector('.wrapper');
+const registerLink = document.querySelector('.register-link');
+const loginLink = document.querySelector('.login-link');
+
+
+registerLink.onclick = () => {
+    wrapper.classList.add('active');
+}
+loginLink.onclick = () => {
+    wrapper.classList.remove('active');
+}
+
+
+  // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyBgwi0qmlNnkzcu1u1Uf_8-8e1IVdHH9Ek",
+
+    authDomain: "login-example-f5b9e.firebaseapp.com",
+
+    projectId: "login-example-f5b9e",
+
+    storageBucket: "login-example-f5b9e.firebasestorage.app",
+
+    messagingSenderId: "207190953884",
+
+    appId: "1:207190953884:web:139d0e8b4fba8a450cc259"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
